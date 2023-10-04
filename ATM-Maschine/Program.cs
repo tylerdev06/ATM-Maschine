c97c161885340141aa57cb90f0bce9cf993fcde6
Tyler <116007320+tylerdev06@users.noreply.github.com>
2023-10-04 22:39:22 +0300

    Update Program.cs
    Frag nach ob man eine Karte besitzt.

--- ATM-Maschine/Program.cs
+++ ATM-Maschine/Program.cs
@@ -1,38 +1,51 @@
-﻿int erwarteteKartennummer = 123456789;
-int erwarteterPin = 1234;
-
-Console.WriteLine("Willkommen bei der ATM-Maschine");
-Console.WriteLine("Bitte geben Sie Ihre Kartennummer ein:");
-string kartenummerEingabe = Console.ReadLine();
-int kartenummer;
-
-if (int.TryParse(kartenummerEingabe, out kartenummer) && kartenummer == erwarteteKartennummer)
+internal class Program
 {
-    Console.WriteLine("Ihre Kartennummer wurde erfolgreich überprüft!");
+    private static void Main(string[] args)
+    {
+        string erwarteteKartennummer = "123456789"; 
+        string erwarteterPin = "1234";
 
-}
-else
-{
-    Console.WriteLine("Ihre Kartennummer ist falsch!");
-    return;
-}
+        Console.WriteLine("Haben Sie schon einen Account? (ja/nein)");
+        string accountabfrage = Console.ReadLine();
 
-Console.WriteLine("Bitte geben Sie Ihre PIN ein:");
-string pinEingabe = Console.ReadLine();
-int pin;
+        if (accountabfrage  == "nein")
+        {
+            Console.WriteLine("Dann beantragen Sie eine Kreditkarte.");
+            return;
+        } 
 
-if (int.TryParse(pinEingabe, out pin) && pin == erwarteterPin)
-{
-    Console.WriteLine("Ihre PIN wurde erfolgreich überprüft!");
-}
-else
-{
-    Console.WriteLine("Ihre PIN ist falsch!");
-    return;
-}
+        Console.WriteLine("Willkommen bei der ATM-Maschine");
+        Console.WriteLine("Bitte geben Sie Ihre Kartennummer ein:");
+        string kartenummerEingabe = Console.ReadLine();
 
-Console.WriteLine("Bitte geben Sie den Betrag ein, den Sie abheben möchten:");
-string betragEingabe = Console.ReadLine();
-Console.WriteLine("Bitte warten Sie, während wir Ihre Daten überprüfen...");
-Console.WriteLine("Ihre Daten wurden erfolgreich überprüft!");
-Console.WriteLine("Ihr Aktueller Kontostand ist: " + betragEingabe);
+        if (kartenummerEingabe == erwarteteKartennummer)
+        {
+            Console.WriteLine("Ihre Kartennummer wurde erfolgreich überprüft!");
+        }
+        else
+        {
+            Console.WriteLine("Ihre Kartennummer ist falsch!");
+            return;
+        }
+
+        Console.WriteLine("Bitte geben Sie Ihre PIN ein:");
+        string pinEingabe = Console.ReadLine();
+
+        if (pinEingabe == erwarteterPin)
+        {
+            Console.WriteLine("Ihre PIN wurde erfolgreich überprüft!");
+        }
+        else
+        {
+            Console.WriteLine("Ihre PIN ist falsch!");
+            return;
+        }
+
+        Console.WriteLine("Bitte geben Sie den Betrag ein, den Sie abheben möchten:");
+        string betragEingabe = Console.ReadLine();
+
+        Console.WriteLine("Bitte warten Sie, während wir Ihre Daten überprüfen...");
+        Console.WriteLine("Ihre Daten wurden erfolgreich überprüft!");
+        Console.WriteLine("Ihr Aktueller Kontostand ist: " + betragEingabe);
+    }
+}
